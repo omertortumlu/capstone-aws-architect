0aaeb3a1cc5442f93dd247bff8520ed5591b7925
omertortumlu <omertortumlu@gmail.com>
2023-03-11 14:42:13 -0500

    userdata

--- userdata.sh
+++ userdata.sh
@@ -9,7 +9,7 @@ cd /home/ubuntu/Capstone_project
 apt install python3-pip -y
 apt-get install python3.7-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR PRIVATE REPO NAME>/src
+cd /home/ubuntu/Capstone_project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
